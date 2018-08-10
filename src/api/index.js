f206d8b78a8b2e119080d86e17580b9407148e72
Khoa <lordknight@LKs-MacBook-Pro.local>
2018-08-10 20:04:45 +0900

    toi

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import Api from './Api';
 
-// const api = new Api('https://emmapi.herokuapp.com');
-const api = new Api('http://127.0.0.1:4000');
+// const api = new Api('http://127.0.0.1:4000');
+const api = new Api('http://147.46.118.35:4000');
 export default api;
